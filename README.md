0e17f7690da7e1a355dbb761464362aa2d2b267f
Christoffer Hallas <challas@liveintent.com>
2021-06-23 11:22:46 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Slack Webhook Action
+
+This action will perform a POST request to a given Slack webhook. This can be
+used to push both simple messages as well as more complex Block Kit based rich
+messages.
+
+## Inputs
+
+### `payload`
+
+**required** The JSON payload to be sent to the webhook. Must be a string.
+
+## Example usage
+
+```
+uses: liveintent/slack-webhook-action@v1.0.0
+with:
+    payload |
+        {
+            "text": "Hello world"
+        }
+```
+
+## Development
+
+In order to develop this project, simply make changes to the action.js file and
+run `npm run build` to create the new distribution in the dist/ folder.
+
+## License
+
+The MIT License
+
+Copyright 2021 LiveIntent, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ action.js
@@ -0,0 +1,56 @@
+/**
+ * The MIT License
+ *
+ * Copyright 2021 LiveIntent, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const core = require('@actions/core')
+const axios = require('axios')
+
+// Look for a string that matches base URL of a Slack webhook but is ending.
+// This will eliminate most cases of fundamentally invalid webhook URLs.
+const validWebhookUrlPattern = /^https:\/\/hooks\.slack\.com\/services\/(?!$)/
+
+try {
+    const webhookUrl = process.env.SLACK_WEBHOOK_URL
+    if (!webhookUrl) throw new Error('The `SLACK_WEBHOOK_URL` environment variable is required')
+
+    if (!validWebhookUrlPattern.test(webhookUrl)) throw new Error('Webhook must be a Slack webhook URL')
+
+    const payload = core.getInput('payload')
+    if (payload.length < 1) throw new Error('The payload input must be supplied')
+
+    let data
+
+    try {
+        data = JSON.parse(payload)
+    } catch (_) {
+        throw new Error('The playoad input must be valid JSON');
+    }
+
+    axios.post(webhookUrl, data)
+        .then(() => {})
+        .catch(err => {
+            throw new Error(`Webhook request failed\n${data}\n${err}`)
+        })
+} catch (err) {
+    core.setFailed(err.message)
+}
